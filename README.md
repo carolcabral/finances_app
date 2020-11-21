baadeb447fe9549624ee5ba117a3b8c1612fcd75
carolcabral <ana_carolina_pimentel@hotmail.com>
2020-11-21 20:49:57 -0300

    Categories in add_expense

--- README.md
+++ README.md
@@ -1,22 +1,42 @@
-
-
-
+- [ ] Lógica para armazenar categorias na base de dados (como pegar parent de optgroup?)
+    - [ ] Trasformar input in camelcase (impedir duplicidade)
+- [ ] Gerenciar views (select input categorias, revenue/expense)
 - [ ] Add_revenue
-- [ ] Linkar página de update na linha inteira da tabela
+    - [ ] Como trabalhar com mais de uma tabela no SQLAlchemy
+    - [ ] Categorias de receita
+- [ ] Flash message para adicionar nova categoria
+- [ ] Ajustes de rota para categorias (mudar de admin)
+- [ ] Ajuste de rota pra index (armazenar mês e ano)
+- [ ] Quando isFixed, atualizar meses para frente (criar nova tabela com fk? se add despesa, atualiza sozinha?)
+
+
+- [ ] Gerar relatórios com plotly
+    - [ ] Escolher intervalo
+    - [ ] Fixo vs variável
+    - [ ] Pie por parent tag
+        - [ ] Pie por child tag dentro da parent tag
+
+
 - [ ] Controle de erros
+    - [ ] Retornar mensagem
     - [ ] Flash message se campos não estiverem preenchidos
         - [ ] Add_expense
         - [ ] Report
         - [ ] Update
         - [ ] Categoria já adicionada
-- [ ] Gerar relatórios para mais de um intervalo
-- [ ] Gerar relatorios de despesas fixas
-- [ ] Gráficos de relatórios
-    - [ ] Fixo vs variável
-    - [ ] Pie por parent tag
-    - [ ] Pie por child tag dentro da parent tag
+
+
+- [ ] Remover categorias (base de dados depois?)
+## Style
+
+- [ ] Navbar
+- [ ] Prev / Next buttons
+- [ ] Delete Icon
+- [ ] Linkar página de update na linha inteira da tabela
 
 ## Version control 
+21-nov | Added categories in add_expense
+- [x] Categorias com input select
 
 20-nov | Input type data, isFixed, início de relatórios e categorias e bug fixes 
 - [x] Campo para despesas fixas
@@ -24,14 +44,8 @@
     - [x] Separar por mês (na base de dados e nas páginas)
     - [x] Ordenar por data
     - [x] Limitar meses até 12
-        - [ ] Otimizar limites
     - [x] Armazenar valor da data no update
     - [x] Tags e categorias como json
-    - [ ] Remover categorias
-    - [ ] Relacionar na base de dados e no cálculos
-    - [ ] Trasformar input in camelcase
-
-
 19-nov | Initial commit 
 - [x] Criar base de dados;
 - [x] Funções de adicionar, alterar e deletar despesas;
--- finances.py
+++ finances.py
@@ -14,7 +14,8 @@ db = SQLAlchemy(app)
 current_month = 11
 current_year = 2020
 
-categories = []
+global categories
+
 class Expenses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.String(4), nullable=False)
@@ -28,22 +29,15 @@ class Expenses(db.Model):
 @app.route("/", methods=['POST', 'GET'])
 def index():
     global current_month, current_year
-    '''
-    if request.method == 'POST': and request.form['current-month'] == 'next':
-            current_month = current_month + 1
-    elif request.method == 'POST' and request.form['current-month'] == 'previous':
-            current_month = current_month - 1
-    '''    
+    
     if request.method == 'POST': 
         if request.form['current-month'] == 'next':
-            # TODO: Otimizar:
             if current_month == 12: 
                 current_month = 1
                 current_year = current_year + 1
             else :
                 current_month = current_month + 1
         else:
-            # TODO: Otimizar:
             if current_month == 1: 
                 current_month = 12
                 current_year = current_year - 1
@@ -51,31 +45,8 @@ def index():
                 current_month = current_month - 1
 
     expenses = Expenses.query.filter_by(month =str(current_month).zfill(2), year=current_year).order_by(Expenses.date).all()
-    
-    print(calculate_balance(expenses))
-       
     return render_template('index.html', expenses=expenses)
 
-
-@app.route("/expense", methods=['POST', 'GET'])
-def add_expense():
-    if request.method == 'POST':
-        print(request.form)
-        year, month, date = request.form['date'].split('-')
-        expense_content = request.form['content']
-        expense_value = request.form['value']
-        expense_isFixed = True if 'isFixed' in request.form else False
-        new_expense = Expenses(year=year, month=month, date=date, content=expense_content, value=expense_value, isFixed=expense_isFixed)
-
-        try:
-            db.session.add(new_expense)
-            db.session.commit()
-            return redirect('/')
-        
-        except:
-            return render_template('error.html', error="There was an issue adding your task")
-    else: 
-        return render_template('add_expense.html')
 
 @app.route("/delete/<int:id>")
 def delete(id):
@@ -91,6 +62,8 @@ def delete(id):
 @app.route("/update/<int:id>", methods=['POST', 'GET'])
 def update(id):
     expense = Expenses.query.get_or_404(id)
+    with open('categories.json', 'r') as f:
+        categories = json.loads(f.read())
     
     if request.method == 'POST':
         expense.year, expense.month, expense.date = request.form['date'].split('-')
@@ -105,7 +78,7 @@ def update(id):
             return render_template('error.html', error='There was an issue updating your tasks')
     
     else:
-        return render_template('update.html', expense=expense)
+        return render_template('update.html', expense=expense, categories=categories)
 
 
 
@@ -113,12 +86,7 @@ def update(id):
 def reports():
     report_year = current_year
     report_month = current_month
-
-    print("-----REQUEST--------")
-    print(request)
-    print("-----FORM--------")
-    print(request.form)
-    
+  
     if request.method == 'POST':
         report_year, report_month = request.form['report-from'].split('-')
         print(report_year)
@@ -130,7 +98,7 @@ def reports():
 
 @app.route("/admin", methods=['POST', 'GET'])
 def admin():
-    global categories
+    #global categories
     with open('categories.json', 'r') as f:
         categories = json.loads(f.read())
     
@@ -141,16 +109,37 @@ def admin():
         print ('PARENT:' + request.form['parent-tag'])
         if request.form['parent-tag'] in categories.keys():
             if request.form['child-tag'] not in categories[request.form['parent-tag']]:
-                #print("Filho ja adc")
-            #else:
                categories[request.form['parent-tag']].append(request.form['child-tag']) 
         else:
             categories[request.form['parent-tag']] = []
             categories[request.form['parent-tag']].append(request.form['child-tag'])
 
         with open('categories.json', 'w') as f:
-            json.dump(categories, f, indent=4)  
+            json.dump(categories, f, indent=4) 
     return render_template('admin.html', categories = categories)
+
+@app.route("/expense", methods=['POST', 'GET'])
+def add_expense():
+    with open('categories.json', 'r') as f:
+        categories = json.loads(f.read())
+    
+
+    if request.method == 'POST':
+        year, month, date = request.form['date'].split('-')
+        expense_content = request.form['content']
+        expense_value = request.form['value']
+        expense_isFixed = True if 'isFixed' in request.form else False
+        new_expense = Expenses(year=year, month=month, date=date, content=expense_content, value=expense_value, isFixed=expense_isFixed)
+
+        try:
+            db.session.add(new_expense)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            return render_template('error.html', error="There was an issue adding your task")
+    else: 
+        return render_template('add_expense.html', categories=categories)
 
 if __name__ == "__main__":
 	db.create_all()
